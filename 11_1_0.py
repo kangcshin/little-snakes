70f785cb6e30fa4f48a81c51f554a1cabb5f202f
Kang <kangcshin@gmail.com>
2019-11-01 10:47:15 -0400

    11-1

---
+++ 11_1_0.py
@@ -0,0 +1,19 @@
+def moveZeroes(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    pointer = 0
+    
+    for num in nums:
+        if num != 0:
+            nums[pointer] = num
+            pointer += 1
+    while pointer < len(nums):
+        nums[pointer] = 0
+        pointer += 1
+
+array = [0,1,0,3,12]
+print(array)
+moveZeroes(array)
+print(array)
