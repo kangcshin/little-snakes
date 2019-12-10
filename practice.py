9aef881d30e04091b57d5b4ef7797c6137e4f740
Kang Shin <kangcshin@gmail.com>
2019-12-10 13:44:52 -0500

    .

--- practice.py
+++ practice.py
@@ -50,3 +50,116 @@ print(numIslands(island_map))
 
 ############################################################################################################
 
+'''
+A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+
+Example 1:
+
+Input: S = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+
+Note:
+
+    S will have length in range [1, 500].
+    S will consist of lowercase letters ('a' to 'z') only.
+
+'''
+
+def partitionLabels(S):
+    """
+    :type S: str
+    :rtype: List[int]
+    """
+    dic = {}
+    for i, char in enumerate(S):
+        dic[char] = i
+    res = []
+    currLen = 0
+    hi = 0
+    for i, char in enumerate(S):
+        hi = max(hi, dic[char])
+        if i == hi:
+            res.append(i - currLen + 1)
+            currLen = i+1
+    return res
+
+S = "ababcbacadefegdehijhklij"
+print(partitionLabels(S))
+
+############################################################################################################
+
+'''
+There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+    If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+    Otherwise, it becomes vacant.
+
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+ 
+
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+
+'''
+
+def prisonAfterNDays(cells, N):
+    def nextday(cells):
+        newcells =[]
+        newcells.append(0)
+        for i in range(1, len(cells)-1):
+            if cells[i - 1] == cells[i + 1]:
+                newcells.append(1)
+            else:
+                newcells.append(0)
+        newcells.append(0)
+        return newcells
+
+    seen = {}
+    while N > 0:
+        c = tuple(cells)
+        if c in seen:
+            N %= seen[c] - N
+        seen[c] = N
+
+        if N >= 1:
+            N -= 1
+            cells = nextday(cells)
+
+    return list(cells)
+
+cells = [0,1,0,1,1,0,0,1]
+N = 7
+
+# cells = [1,0,0,1,0,0,1,0]
+# N = 1000000000
+print(prisonAfterNDays(cells, N))
+
+############################################################################################################
