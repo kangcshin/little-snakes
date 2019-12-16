e63dbd195d62569eb37617825e9328ed8938ef17
Kang Shin <kangcshin@gmail.com>
2019-12-16 11:29:22 -0500

    .

--- practice.py
+++ practice.py
@@ -418,4 +418,47 @@ s = "awaglknagawunagwkwagl"
 k = 4
 print(substringk(s, k))
 ############################################################################################################
+'''
+Given a string s and an int k, return an int representing the number of substrings (not unique) of s with exactly k distinct characters. If the given string doesn't have k distinct characters, return 0.
+https://leetcode.com/problems/subarrays-with-k-different-integers
+
+Example 1:
+
+Input: s = "pqpqs", k = 2
+Output: 7
+Explanation: ["pq", "pqp", "pqpq", "qp", "qpq", "pq", "qs"]
+
+Example 2:
+
+Input: s = "aabab", k = 3
+Output: 0
+
+Constraints:
+
+    The input string consists of only lowercase English letters [a-z]
+    0 ≤ k ≤ 26
+
+'''
+
+def subStrings(s, k):
+    if k > len(set(s)):
+        return 0
+    
+    result = 0
+    
+    for i in range(len(s)):
+        dist = set()
+        for j in range(i, len(s)):
+            if s[j] not in dist:
+                dist.add(s[j])
+            if len(dist) == k:
+                result += 1
+            elif len(dist) > k:
+                break
+    return result
+
+s = 'pqpqs'
+k = 2
+
+print(subStrings(s, k))
 ############################################################################################################
