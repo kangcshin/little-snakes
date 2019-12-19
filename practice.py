8c00f54d1af6080843a56fc710ff7fd07fdaf076
Kang Shin <kangcshin@gmail.com>
2019-12-19 13:44:12 -0500

    .

--- practice.py
+++ practice.py
@@ -503,6 +503,58 @@ class Solution:
         
         return s is t
 ############################################################################################################
+'''
+Given an N-ary tree, find the subtree with the maximum average. Return the root of the subtree.
+A subtree of a tree is the node which have at least 1 child plus all its descendants. The average value of a subtree is the sum of its values, divided by the number of nodes.
+
+Example 1:
+
+Input:
+		 20
+	   /   \
+	 12     18
+  /  |  \   / \
+11   2   3 15  8
+
+Output: 18
+Explanation:
+There are 3 nodes which have children in this tree:
+12 => (11 + 2 + 3 + 12) / 4 = 7
+18 => (18 + 15 + 8) / 3 = 13.67
+20 => (12 + 11 + 2 + 3 + 18 + 15 + 8 + 20) / 8 = 11.125
+
+18 has the maximum average so output 18.
+
+'''
+
+
+
+def maxAverageSubtree(root):
+    if not root or not roo.children:
+        return None
+    
+    # averate, number of nodes
+    result = [float('-inf'), 0]
+    dfs(root)
+    
+    def dfs(node):
+        if not node.children:
+            return [node.val, 1]
+
+        temp_sum, temp_num = node.val, 1
+        for child in node.children:
+            child_sum, child_num = dfs(child)
+            temp_sum += child_sum
+            temp_num += child_num
+
+        if temp_sum/temp_num > result [0]:
+            result = [temp_sum/temp_num, root.val]
+        
+        return [temp_sum, temp_num]
+    
+   
+    return result[1]
+
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
