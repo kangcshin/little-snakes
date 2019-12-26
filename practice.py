e515edd0c5ad019bf252451fa3d8e68dcd949b1d
Kang Shin <kangcshin@gmail.com>
2019-12-26 11:09:00 -0500

    .

--- practice.py
+++ practice.py
@@ -886,6 +886,24 @@ print(zombie(input_matrix))
                 ground = min(height[low], height[high])
         return rain
 ############################################################################################################
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) >= self.capacity and key not in self.cache:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
 ############################################################################################################
 
 
