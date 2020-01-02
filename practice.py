e029f5ee94d4a2e5cae6e33fccc582b9d3ad6290
Kang Shin <kangcshin@gmail.com>
2020-01-02 13:09:59 -0500

    .

--- practice.py
+++ practice.py
@@ -975,6 +975,24 @@ class Solution:
             last.next = last = ListNode(n % 10)
         return first
 ############################################################################################################
+from collections import OrderedDict
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if len(self.cache) >= self.capacity and key not in self.cache:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
+        self.cache.move_to_end(key)
 ############################################################################################################
 ############################################################################################################
 ############################################################################################################
