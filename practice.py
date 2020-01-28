cf0d54cff6c31cd8d6976f8ed71c0102557561cc
Kang Shin <kangcshin@gmail.com>
2020-01-27 19:24:19 -0500

    .

--- practice.py
+++ practice.py
@@ -1585,4 +1585,18 @@ class Solution:
         
         return helper(left, right)
     
-    
+    class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        output = []
+        
+        for interval in intervals:
+            if not output or output[-1][1] < interval[0]:
+                output.append(interval)
+            else:
+                output[-1][1] = max(output[-1][1], interval[1])
+
+                
+        return output
+
+
